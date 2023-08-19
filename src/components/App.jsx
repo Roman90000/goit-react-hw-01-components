6d948ba6b7e6962911ff9a56bd08247c6901516d
Zhuk Roman <95108009+Roman90000@users.noreply.github.com>
2023-08-19 23:24:27 +0300

    process of filling the App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,14 @@
 import { Profile } from "./Profile";
+import { Statistics } from "./Statistics";
+import user from "../user.json";
+import data from "../data.json";
+
 
 export const App = () => {
   return (
     <div>
-      <Profile/>
+      <Profile items={user} />
+      <Statistics items={data} />
     </div>
   );
 };
---
+++ src/components/Statistics.js
@@ -0,0 +1,5 @@
+export const Statistics = () => {
+    return<section class="statistics">
+        <h2 class="title">Upload stats</h2>
+    </section>
+};
